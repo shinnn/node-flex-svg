238281e86cf2a80229ada29d07068ed4eebf4061
shinnn <snnskwtnb@gmail.com>
2014-03-28 03:28:27 +0900

    improve task composition

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,21 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var mocha = require('gulp-mocha');
+var es6transpiler = require('gulp-es6-transpiler');
+
+// mocha
+var GLOBALS = {
+  describe: false,
+  it: false,
+  specify: false,
+  before: false,
+  beforeEach: false,
+  after: false,
+  afterEach: false
+};
 
 gulp.task('lint', function() {
-  return gulp.src(['*.js', 'test/test.js'])
+  return gulp.src(['{,src/,test/}*.js'])
     .pipe(jshint({
       camelcase: true,
       trailing: true,
@@ -15,27 +27,30 @@ gulp.task('lint', function() {
       globalstrict: true,
       browser: false,
       node: true,
-      // mocha
-      globals: {
-        describe: false,
-        it: false,
-        specify: false,
-        before: false,
-        beforeEach: false,
-        after: false,
-        afterEach: false
-      }
+      esnext: true,
+      globals: GLOBALS
     }))
     .pipe(jshint.reporter(stylish));
 });
 
-gulp.task('test', function() {
-  return gulp.src(['test/test.js'])
+gulp.task('transpile', function(cb) {
+  gulp.src(['src/*.js'])
+    .pipe(es6transpiler())
+    .pipe(gulp.dest('lib'));
+  gulp.src(['test/test.js'])
+    .pipe(es6transpiler({globals: GLOBALS}))
+    .pipe(gulp.dest('tmp'));
+  cb();
+});
+
+
+gulp.task('watch', function () {
+  gulp.watch(['{,src/,test/}*.js'], ['test']);
+});
+
+gulp.task('test', ['lint', 'transpile'], function() {
+  gulp.src(['tmp/test.js'])
     .pipe(mocha({reporter: 'nyan'}));
 });
 
-gulp.task('watch', function () {
-  gulp.watch(['*.js', 'test/test.js'], ['lint', 'test']);
-});
-
-gulp.task('default', ['lint', 'test', 'watch']);
+gulp.task('default', ['test', 'watch']);
