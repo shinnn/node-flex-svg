170110ceaa94309b549bf309a697abbfa8a7ddb9
shinnn <snnskwtnb@gmail.com>
2014-03-28 03:25:37 +0900

    use ECMAScript 6 syntax

--- src/flex-svg.js
+++ src/flex-svg.js
@@ -4,19 +4,18 @@
 
 'use strict';
 
-var xml2js = require('xml2js');
+let xml2js = require('xml2js');
 var parser = new xml2js.Parser();
 var builder = new xml2js.Builder();
 xml2js = undefined;
 
 module.exports = function flexSvg(data, callback) {
-  parser.parseString(data, function (err, result) {
-    var attributes = result.svg.$;
+  parser.parseString(data, (err, result) => {
+    let attributes = result.svg.$;
     if (attributes) {
       delete attributes.width;
       delete attributes.height;
     }
-    
     callback(err, builder.buildObject(result));
   });
 };
--- test/test.js
+++ test/test.js
@@ -3,44 +3,43 @@
 var fs = require('fs');
 var assert = require('assert');
 var Q = require('q');
-var flexSvg = require('../flex-svg.js');
+var flexSvg = require('../lib/flex-svg.js');
 
 var readFile = Q.denodeify(fs.readFile);
 var promiseFlexSvg = Q.denodeify(flexSvg);
-var flexSvgFile = function(path) {
-  return readFile(path).then(function(res) {
-    return promiseFlexSvg(res);
-  });
+var flexSvgFile = (path) => {
+  return readFile(path)
+    .then(res => promiseFlexSvg(res));
 };
 
-describe('flex-svg', function() {
-  it('should remove width and height attributes from SVG.', function() {
+describe('flex-svg', () => {
+  it('should remove width and height attributes from SVG.', () => {
     return Q.all([
       flexSvgFile('test/fixture/width_and_height.svg'),
       readFile('test/expected/width_and_height.svg')
-    ]).spread(function(actual, expected) {
+    ]).spread((actual, expected) => {
       assert.strictEqual(actual, expected.toString());
     });
   });
 
-  it("should return SVG string even if the input SVG doesn't have any attributes", function() {
+  it("should return SVG string even if the input SVG doesn't have any attributes", () => {
     return Q.all([
       flexSvgFile('test/fixture/no_attr.svg'),
       readFile('test/expected/no_attr.svg')
-    ]).spread(function(actual, expected) {
+    ]).spread((actual, expected) => {
       assert.strictEqual(actual, expected.toString());
     });
   });
   
-  it('should throw an error when input string is not XML.', function() {
-    assert.throws(function() {
-      flexSvg("I'm not XML.", function() {});
+  it('should throw an error when input string is not XML.', () => {
+    assert.throws(() => {
+      flexSvg("I'm not XML.", () => {});
     });
   });
 
-  it('should throw an error when input string is not SVG but XML.', function() {
-    assert.throws(function() {
-      flexSvg("<html></html>", function() {});
+  it('should throw an error when input string is not SVG but XML.', () => {
+    assert.throws(() => {
+      flexSvg("<html></html>", () => {});
     });
   });
 });
