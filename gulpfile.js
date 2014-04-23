b2e13b98c371c7e32747b4e3f09f3b15a927053e
shinnn <snnskwtnb@gmail.com>
2014-04-24 01:53:41 +0900

    run gulp tasks in more sequential way

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
-
+var merge = require('event-stream').merge;
 var stylish = require('jshint-stylish');
 
 // mocha
@@ -17,25 +17,37 @@ var GLOBALS = {
 };
 
 gulp.task('lint', function() {
-  return gulp.src(['{,src/,test/}*.js'])
+  gulp.src(['{,src/,test/}*.js'])
     .pipe($.jshint())
     .pipe($.jshint.reporter(stylish));
+  gulp.src(['package.json'])
+    .pipe($.jsonlint())
+    .pipe($.jshint.reporter());
 });
 
 gulp.task('transpile', function(cb) {
-  gulp.src(['src/*.js'])
-    .pipe($.es6Transpiler())
-    .pipe(gulp.dest('lib'));
-  gulp.src(['test/*.js'])
-    .pipe($.es6Transpiler({globals: GLOBALS}))
-    .pipe($.espower())
-    .pipe(gulp.dest('tmp'));
-  cb();
+  gulp.src(['{lib,tmp}/*'], {read: false})
+    .pipe($.rimraf())
+    .on('finish', function() {
+      merge(
+        gulp.src(['src/*.js'])
+          .pipe($.es6Transpiler())
+          .pipe(gulp.dest('lib')),
+        gulp.src(['test/*.js'])
+          .pipe($.es6Transpiler({globals: GLOBALS}))
+          .pipe($.espower())
+          .pipe(gulp.dest('tmp'))
+      ).on('end', cb);
+    });
 });
 
-
 gulp.task('watch', function() {
-  gulp.watch(['{,src/,test/}*.js'], ['test']);
+  gulp.watch([
+    '{src,test}/*.js',
+    'package.json',
+    '.jshintrc'
+  ], ['test']);
+  gulp.watch(['gulpfile.js'], ['lint']);
 });
 
 gulp.task('test', ['lint', 'transpile'], function() {
